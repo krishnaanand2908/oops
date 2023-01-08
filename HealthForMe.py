c25ca18d3f456e723af5108a6387457a9deb95a9
krishnaanand2908 <krishnaanand290809@gmail.com>
2023-01-08 14:36:22 +0530

    Healthy Programmer

--- HealthForMe.py
+++ HealthForMe.py
@@ -1,5 +1,4 @@
 import os
-import pyttsx3
 import datetime
 import pygame
 import time
@@ -7,8 +6,8 @@ import fontstyle as fnt
 
 def musicPlayer(file, stopper):
     pygame.mixer.init()
-    pygame.mixer.load(file)
-    pygame.mixer.play()
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
     while True:
         user_input = input()
         if user_input == stopper:
@@ -20,24 +19,31 @@ def log(message):
         f.write(f"{message} {datetime.datetime.now()}\n")
         
 if __name__ == "__main__":
+    os.system('cls')
     init_water = time.time()
-    init_water = time.time()
-    init_water = time.time()
+    init_eyes = time.time()
+    init_exercise = time.time()
     waterSecs = 30*60
-    exerSecs = 40*60
-    eyeSecs = 35*60
+    exerSecs = 35*60
+    eyeSecs = 40*60
     
     while True:
-        if (time() - init_water) > waterSecs:
-            print(fnt.apply("Water Drinking time. Enter 'drank' to stop the alarm.", "blue/bold"))
-            musicPlayer('water.mp3', 'drank')
-            init_water = time()
+        if (time.time() - init_water) > waterSecs:
+            print(fnt.apply("Water Drinking time. Enter 'done' to stop the alarm.", "blue/bold"))
+            musicPlayer('water.mp3', 'done')
+            init_water = time.time()
             log("Drank Water at")
     
-        if (time() - init_water) > waterSecs:
-            print(fnt.apply("Water Drinking time. Enter 'drank' to stop the alarm.", "green/bold"))
-            musicPlayer('eye.mp3', 'DoneEyes')
-            init_water = time()
+        if (time.time() - init_eyes) > eyeSecs:
+            print(fnt.apply("Eyes Relaxation time. Enter 'done' to stop the alarm.", "green/bold"))
+            musicPlayer('eyes.mp3', 'done')
+            init_water = time.time()
             log("Eyes relaxed at")
+            
+        if (time.time() - init_exercise) > exerSecs:
+            print(fnt.apply("Physical Activity time. Enter 'done' to stop the alarm.", "yellow/bold"))
+            musicPlayer('exercise.mp3', 'done')
+            init_water = time.time()
+            log("Physical Activity done at")
         
 
